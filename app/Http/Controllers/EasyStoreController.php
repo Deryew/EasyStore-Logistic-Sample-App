3421d335e13dcc7336baaffb73c9aec03e7f1f06
deryew <deryew@easystore.co>
2020-10-20 22:28:53 +0800

    Attempt get cart token, order token and shop url in pickup iframe

--- app/Http/Controllers/EasyStoreController.php
+++ app/Http/Controllers/EasyStoreController.php
@@ -254,9 +254,9 @@ class EasyStoreController extends Controller
 
     public function pickupIFrame(Request $request)
     {
-        // $shop_url = $_SERVER["HTTP_EASYSTORE_SHOP_DOMAIN"];
-        // $cart_token = $request->header('x-easystore-cart-token');
-        // $order_token = $request->header('x-easystore-order-token');
+        $shop_url = $_SERVER["HTTP_EASYSTORE_SHOP_DOMAIN"];
+        $cart_token = $request->header('x-easystore-cart-token');
+        $order_token = $request->header('x-easystore-order-token');
 
         // if(!$shop_url)
         //     return response()->json(["errors" => "Shop not found"], 400);
