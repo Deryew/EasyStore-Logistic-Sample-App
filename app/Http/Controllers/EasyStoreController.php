3eb5b9cd79749c0b7dcfc5ac8c07a1b60661e226
deryew <deryew@easystore.co>
2020-10-20 16:51:55 +0800

    Removed codes in pickip verify rate

--- app/Http/Controllers/EasyStoreController.php
+++ app/Http/Controllers/EasyStoreController.php
@@ -284,16 +284,16 @@ class EasyStoreController extends Controller
     public function pickupVerifyRate(Request $request)
     {
 
-        if ($_SERVER["HTTP_EASYSTORE_TOPIC"] != 'pickup/verify') {
-            return response()->json(['errors' => 'Topic invalid'], 400);
-        }
+        // if ($_SERVER["HTTP_EASYSTORE_TOPIC"] != 'pickup/verify') {
+        //     return response()->json(['errors' => 'Topic invalid'], 400);
+        // }
 
-        $data = file_get_contents('php://input');
-        $hmac = hash_hmac('sha256', $data, $this->app_secret);
+        // $data = file_get_contents('php://input');
+        // $hmac = hash_hmac('sha256', $data, $this->app_secret);
 
-        if ($hmac != $_SERVER["HTTP_EASYSTORE_HMAC_SHA256"]) {
-            return response()->json(['errors' => 'Hmac validate fail'], 400);
-        }
+        // if ($hmac != $_SERVER["HTTP_EASYSTORE_HMAC_SHA256"]) {
+        //     return response()->json(['errors' => 'Hmac validate fail'], 400);
+        // }
 
         // Sample Data
         $pickup_location = [
