6f2d1876c7a23c0377b4c3ce719829c1fbcd3334
deryew <deryew@easystore.co>
2020-10-20 15:02:37 +0800

    Added route get for pickup-rate

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::group(['prefix' => 'easystore'], function () {
     Route::get('/proxy/non-cod', 'EasyStoreController@pickupIFrame');
     // Route::get('/proxy/cod', 'EasyStoreController@pickupIFrame');
     Route::post('/proxy/pickup-rate', 'EasyStoreController@pickupiFrameRate');
-
+    Route::get('/proxy/pickup-rate', 'EasyStoreController@pickupIFrameRate');
 
 
 
