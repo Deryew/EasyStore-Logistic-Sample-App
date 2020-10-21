c2f1ba0c1e27116a3d70cb33d3f49d2d68203692
deryew <deryew@easystore.co>
2020-10-21 09:29:33 +0800

    Added CSS style for non_cod content wrapper

--- resources/views/non_cod_location.blade.php
+++ resources/views/non_cod_location.blade.php
@@ -6,7 +6,16 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" type="text/javascript"></script>
-  <link rel="stylesheet" href="https://s3-ap-southeast-1.amazonaws.com/assets.easystore.co/css/uikit.css">
+    <link rel="stylesheet" href="https://s3-ap-southeast-1.amazonaws.com/assets.easystore.co/css/uikit.css">
+
+    <style>
+        .content-wrapper{
+			padding: 15px;
+			max-width: 450px;
+			margin: 0 auto;
+		}
+    </style>
+
 </head>
 
 <body>
